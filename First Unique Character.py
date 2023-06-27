25a7aea1a9b7c5760c6051f003e06dbd75d71542
Priyal <135606313+priyaltanwar@users.noreply.github.com>
2023-06-27 16:54:58 +0530

    Add files via upload

---
+++ First Unique Character.py
@@ -0,0 +1,10 @@
+def firstUniqChar(s):
+        char_count = {}
+
+        for char in s:
+            char_count[char]= char_count.get(char, 0) + 1
+
+        for i, char in enumerate(s):
+            if char_count[char] == 1:
+                return i
+        return -1
